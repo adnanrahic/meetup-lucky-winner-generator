8f4ef311d16d6c5570817eac7e4372df128b83d8
adnanrahic <ado.raha198@gmail.com>
2018-04-24 10:56:42 +0200

    added usage guide and more detailed winner text

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const meetupLuckyWinnerGenerator = require('./meetupLuckyWinnerGenerator');
 
+// Set the options for what meetup and event you want to get the lucky winner
+const options = { 
+  meetup: 'sarajevo-openweb-meetup', 
+  eventId: 247769521 
+};
+
+// Generate the lucky winner with the given options
 meetupLuckyWinnerGenerator
-  .generateOneLuckyWinner({ meetup: 'sarajevo-openweb-meetup', eventId: 247769521 })
-  .then(luckyWinner => console.log(luckyWinner.member.name));
+  .generateOneLuckyWinner(options)
+  .then(luckyWinner => console.log(`Today's lucky winner is: ${luckyWinner.member.name}!`));
--- readme.md
+++ readme.md
@@ -1,3 +1,11 @@
 # Meetup Lucky Winner Generator
+A simple package for generating lucky winners if there is a free coupon giveaway at a Meetup event!
 
 ## Usage
+- add your API KEY to the `sample.env` file
+- rename the `sample.env` to just `.env`
+- in the `app.js` change the options to your `meetup` and `eventId`
+- open your terminal:
+  - `npm install`
+  - `npm start`
+- The terminal will return the name of the lucky winner
